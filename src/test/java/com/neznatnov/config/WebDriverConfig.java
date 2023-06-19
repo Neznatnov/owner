6beee2b956702fc7eb797d3ea0bad299b6e93dbc
Veronika Iatckaia <neznatnov@gmail.com>
2023-06-19 20:09:33 +0200

    fixed default url and version chrome

--- src/test/java/com/neznatnov/config/WebDriverConfig.java
+++ src/test/java/com/neznatnov/config/WebDriverConfig.java
@@ -20,6 +20,5 @@ public interface WebDriverConfig extends Config {
     String getBrowserVersion();
 
     @Key("remoteUrl")
-    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
     String getRemoteUrl();
 }
